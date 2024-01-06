e850d3c82a4d76906b4ac480e63ed89a3edf97ed
sdongpil <pildong94@naver.com>
2024-01-06 16:07:29 +0900

    프로그래머스 전화번호 목록

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,16 @@
 import baekjoon.Cicle;
+import programmers.Test4;
 
 public class Main {
     public static void main(String[] args) {
 
+        String[] arr = {"12", "1234", "145"};
+        String[] arr2 = {"12", "34", "145"};
 
-        Cicle cicle = new Cicle();
-        cicle.printCircle();
+        Test4 test4 = new Test4();
 
-
+        boolean solution = test4.solution(arr2);
+        System.out.println(solution);
     }
 
 }
